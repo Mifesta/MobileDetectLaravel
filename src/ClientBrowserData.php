b10ba4a21c0d74549c16967be2a90ef529403bb8
Cardee <sergey@letfix.ru>
2024-12-06 16:56:27 +0300

    1.4

--- src/ClientBrowserData.php
+++ src/ClientBrowserData.php
@@ -2,9 +2,10 @@
 
 namespace MifestaClientBrowserData;
 
+use Detection\MobileDetect;
+use Exception;
+
 /**
- * Class ClientBrowserData
- *
  * @method static array getHttpHeaders()
  * @method static array getHttpHeader(string $header)
  * @method static array getMobileHeaders()
@@ -32,17 +33,15 @@ namespace MifestaClientBrowserData;
  * @method static array getMobileDetectionRules()
  * @method static array getOperatingSystems()
  * @method static array getProperties()
- *
- * @see \Mobile_Detect
+ * @see \Detection\MobileDetect
  */
 abstract class ClientBrowserData
 {
     /**
-     * Public methods from \Mobile_Detect
-     *
+     * Public methods from \Detection\MobileDetect
      * @var array
      */
-    private static $valid_functions = [
+    private static array $valid_functions = [
         'getHttpHeaders',
         'getHttpHeader',
         'getMobileHeaders',
@@ -64,11 +63,10 @@ abstract class ClientBrowserData
     ];
 
     /**
-     * Public static methods from \Mobile_Detect
-     *
+     * Public static methods from \Detection\MobileDetect
      * @var array
      */
-    private static $valid_static_functions = [
+    private static array $valid_static_functions = [
         'getScriptVersion',
         'getPhoneDevices',
         'getTabletDevices',
@@ -79,102 +77,83 @@ abstract class ClientBrowserData
         'getOperatingSystems',
         'getProperties',
     ];
-
     /**
-     * @var \Mobile_Detect
+     * @var \Detection\MobileDetect
      */
-    private static $_mobile_detect_class;
-
+    private static MobileDetect $_mobile_detect_class;
     /**
      * Magic method __callStatic for ClientBrowserData
-     *
      * @param string $name
      * @param array $arguments
-     *
      * @return mixed
      * @throws \Exception
      */
-    public static function __callStatic($name, $arguments)
+    public static function __callStatic(string $name, array $arguments)
     {
         if (in_array($name, self::$valid_functions)) {
             return call_user_func_array([self::getMobileDetect(), $name], $arguments);
         } elseif (in_array($name, self::$valid_static_functions)) {
-            return call_user_func_array([\Mobile_Detect::class, $name], $arguments);
+            return call_user_func_array([MobileDetect::class, $name], $arguments);
         } else {
-            throw new \Exception('Call to undefined methods '.__CLASS__.'::'.$name.'()');
+            throw new Exception('Call to undefined methods '.__CLASS__.'::'.$name.'()');
         }
     }
 
     /**
      * Check if the device is mobile.
      * Returns true if any type of mobile device detected, including special ones
-     *
      * @return bool
      */
-    public static function isMobile()
+    public static function isMobile(): bool
     {
         return self::getMobileDetect()->is('MobileBot') || self::getMobileDetect()->isMobile();
     }
 
     /**
      * This method checks for a 'Bot' property in the userAgent.
-     *
      * @return bool
      */
-    public static function isAnyBot()
+    public static function isAnyBot(): bool
     {
-        return preg_match('#(bot|spider|yeti|ichiro)#is', self::getMobileDetect()->getUserAgent()) || self::getMobileDetect()->is('MobileBot') || self::getMobileDetect()->is('Bot');
+        $classMobileDetect = self::getMobileDetect();
+        return !($user_agent = $classMobileDetect->getUserAgent() ?? '') || preg_match('#(bot|spider|yeti|ichiro)#i', $user_agent) || $classMobileDetect->is('MobileBot') || $classMobileDetect->is('Bot');
     }
 
     /**
      * This method checks for a 'Bot' property in the userAgent.
      * Does not include mobile bots.
-     *
      * @return bool
      */
-    public static function isBot()
+    public static function isBot(): bool
     {
         $classMobileDetect = self::getMobileDetect();
-        $user_agent = $classMobileDetect->getUserAgent();
-
-        return $classMobileDetect->is('Bot') || preg_match('#(yeti|ichiro)#is', $user_agent) || (preg_match('#(bot|spider)#is', $user_agent) && ! $classMobileDetect->is('MobileBot'));
+        return !($user_agent = $classMobileDetect->getUserAgent()) || $classMobileDetect->is('Bot') || preg_match('#(yeti|ichiro)#i', $user_agent) || (preg_match('#(bot|spider)#i', $user_agent) && ! $classMobileDetect->is('MobileBot'));
     }
 
     /**
      * This method checks for a certain property in the userAgent.
-     *
      * @param string $key
-     *
-     * @return bool|int|null
+     * @return bool
      */
-    public static function is($key)
+    public static function is(string $key): bool
     {
-        switch ($key) {
-            case 'Bot':
-                return self::isBot();
-                break;
-            case 'Mobile':
-                return self::isMobile();
-                break;
-            case 'AnyBot':
-                return self::isAnyBot();
-                break;
-            default:
-                return self::getMobileDetect()->is($key);
-        }
+        return match ($key) {
+            'Bot' => self::isBot(),
+            'Mobile' => self::isMobile(),
+            'AnyBot' => self::isAnyBot(),
+            default => self::getMobileDetect()->is($key),
+        };
     }
 
     /**
-     * Get class \Mobile_Detect
-     *
-     * @return \Mobile_Detect
+     * Get class \Detection\MobileDetect
+     * @return \Detection\MobileDetect
      */
-    private static function getMobileDetect()
+    private static function getMobileDetect(): MobileDetect
     {
         if (! self::$_mobile_detect_class) {
-            self::$_mobile_detect_class = new \Mobile_Detect($_SERVER, request()->header('User-Agent'));
+            self::$_mobile_detect_class = new MobileDetect($_SERVER, request()->header('User-Agent'));
         }
-
         return self::$_mobile_detect_class;
     }
 }
